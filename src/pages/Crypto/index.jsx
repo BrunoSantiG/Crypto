99d54dca23bd77c0c4622231ad10434f8f937528
Bruno Santi Garcia <bruno.santi.98@outlook.com>
2020-09-24 16:06:14 -0300

    [+] Add info cards at crypto page

--- src/pages/Crypto/index.jsx
+++ src/pages/Crypto/index.jsx
@@ -1,13 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { getReduxCryptos } from '../../store/reducers/crypto';
+import Cards from '../../components/Cards';
 import { get24HChart } from '../../services/api';
 import Chart from './Chart';
 
-import { Container, TopRow, PrevArea, PrevIcon, Title } from './styles';
+import {
+	Container,
+	TopRow,
+	PrevArea,
+	PrevIcon,
+	Title,
+	CardBox,
+} from './styles';
+import { filterCrypto } from './util';
 
 function Crypto() {
 	let { crypto } = useParams();
 	const [chartData, setChartData] = useState([]);
+	const response = useSelector((state) => {
+		return state.crypto;
+	});
+	const dispatch = useDispatch();
+	let obj;
 	useEffect(() => {
 		setChartData([]);
 		async function handleChartData() {
@@ -16,9 +33,16 @@ function Crypto() {
 				setChartData(response.data);
 			}
 		}
+		if (response.data.length === 0) {
+			dispatch(getReduxCryptos());
+		}
 		handleChartData();
-	}, [crypto]);
+	}, [crypto, dispatch, response.data]);
 
+	if (response.data.length > 0) {
+		obj = filterCrypto(crypto, response.data);
+	}
+	const sufix = crypto.split('_')[0];
 	return (
 		<Container>
 			<TopRow>
@@ -30,8 +54,50 @@ function Crypto() {
 
 				<Title>{crypto}</Title>
 			</TopRow>
-			{chartData && (
-				<Chart data={chartData} yAxis={crypto.split('_')[0]} />
+			{chartData && <Chart data={chartData} yAxis={sufix} />}
+			{obj && (
+				<CardBox>
+					<Cards
+						title={'Last Price'}
+						content={obj.last}
+						sufix={sufix}
+					/>
+					<Cards title={'Variation'} content={obj.percentChange} />
+					<Cards
+						title={'Highest Bid'}
+						content={obj.highestBid}
+						sufix={sufix}
+					/>
+					<Cards
+						title={'Lowest Ask'}
+						content={obj.lowestAsk}
+						sufix={sufix}
+					/>
+				</CardBox>
+			)}
+			{obj && (
+				<CardBox>
+					<Cards
+						title={'Volume'}
+						content={obj.baseVolume}
+						sufix={sufix}
+					/>
+					<Cards
+						title={'Quote Volume'}
+						content={obj.quoteVolume}
+						sufix={sufix}
+					/>
+					<Cards
+						title={'24h High'}
+						content={obj.high24hr}
+						sufix={sufix}
+					/>
+					<Cards
+						title={'24h Low'}
+						content={obj.low24hr}
+						sufix={sufix}
+					/>
+				</CardBox>
 			)}
 		</Container>
 	);
--- src/styles/themes/light.js
+++ src/styles/themes/light.js
@@ -6,5 +6,6 @@ export default {
 		secondaryHover: '#e1e1e1',
 		background: '#f7f7f7',
 		text: '#333',
+		shadow: 'rgba(0,0,0,0.2)',
 	},
 };
