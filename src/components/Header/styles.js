3a7f1119c5e778cf72214f6fad97c171279dd36f
Bruno Santi Garcia <bruno.santi.98@outlook.com>
2020-09-24 08:36:14 -0300

    [+] Add Github Icon that redirects to repo

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-
+import { Github } from 'styled-icons/feather';
 export const Container = styled.header`
 	height: 60px;
 	background: var(--secondary);
@@ -22,4 +22,31 @@ export const LogoArea = styled.div`
 		font: 400 2rem Archivo;
 		color: var(--logo);
 	}
+`;
+export const RightArea = styled.div`
+	display: flex;
+	align-items: center;
+`;
+
+export const ExternalLink = styled.a`
+	margin-right: 1rem;
+	background: var(--secondary-hover);
+	height: 50px;
+	width: 50px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	border-radius: 50%;
+	cursor: pointer;
+	transition: all ease-in-out 0.3s;
+
+	&:hover {
+		transform: scale(0.91);
+	}
+`;
+export const GithubIcon = styled(Github)`
+	flex-shrink: 0;
+	width: 30px;
+	height: 30px;
+	color: var(--text);
 `;
